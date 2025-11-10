3c1eceb29352194c50605173ed929380dbead943
akwasnik <adrian.kwasnik@gmail.com>
2025-11-10 19:15:35 +0100

    initalized migration from csv database to mongo

--- mangaManager/src/main/java/com/mmp/mangaManager/service/MangaService.java
+++ mangaManager/src/main/java/com/mmp/mangaManager/service/MangaService.java
@@ -4,53 +4,18 @@ import com.mmp.mangaManager.domain.Manga;
 import com.mmp.mangaManager.domain.enums.Status;
 import org.springframework.stereotype.Service;
 
-import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.StandardOpenOption;
 import java.util.*;
-import java.util.stream.Collectors;
 
 @Service
 public class MangaService {
-    private static final Path DB_PATH = Path.of("db/mangas.csv");
-    private List<Manga> mangas = loadMangasFromCsv();
-
-    private List<Manga> loadMangasFromCsv() {
-        try (BufferedReader br = Files.newBufferedReader(DB_PATH)) {
-            return br.lines()
-                    .skip(1)
-                    .filter(line -> !line.trim().isEmpty())
-                    .map(line -> {
-                        String[] data = line.split(",");
-                        Integer id = Integer.parseInt(data[0]);
-                        String title = data[1];
-                        String author = data[2];
-                        Integer year = Integer.parseInt(data[3]);
-                        Status status = Status.valueOf(data[4]);
-                        return new Manga(id, title, author, year, status);
-                    })
-                    .collect(Collectors.toList());
-        } catch (IOException e) {
-            throw new RuntimeException("Błąd przy wczytywaniu CSV: " + e.getMessage(), e);
-        }
-    }
+    private List<Manga> mangas = new ArrayList<>();
 
     public List<Manga> getAllMangas() {
         return mangas;
     }
 
     public void addManga(String title, String author, Integer year, Status status) {
-        Integer id = mangas.size() + 1;
-        String line = id + "," + title + "," + author + "," + year + "," + status;
-        try (BufferedWriter writer = Files.newBufferedWriter(DB_PATH, StandardOpenOption.APPEND)) {
-            writer.newLine();
-            writer.write(line);
-            mangas.add(new Manga(id, title, author, year, status));
-            System.out.println("Dodano mange: " + title);
-        } catch (IOException e) {
-            throw new RuntimeException("Błąd zapisu CSV: " + e.getMessage(), e);
-        }
+
     }
 
     public static void main(String[] args) {
